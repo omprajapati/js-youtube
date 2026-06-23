3b040056368ba707f41c19285b640566625517a1
omprajapati <omprajapati@gmail.com>
2026-06-23 11:43:42 +0545

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3);    // we are using node js, not a browser
+
+// console.log(3 + 3);
+// console.log("Hello"); // code readability should be high
+
+// data types
+
+let name = "sanju";
+let age = 36;
+let isLoggedIn = false;
+
+// numbers => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true / false
+// null => standalone value, this is not undefined, not zero,
+// undefined =>
+// symbol => use for uniqueness
+
+// object
+console.log(typeof name); // string
+console.log(typeof age);    // number
+console.log(typeof null);   // object
+console.log(typeof undefined); //undefined
