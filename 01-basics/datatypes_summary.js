a1125c6630d40f826e618fdaf43f37e74d9e4373
omprajapati <omprajapati@gmail.com>
2026-06-23 13:52:27 +0545

    data type summary

---
+++ 01-basics/datatypes_summary.js
@@ -0,0 +1,44 @@
+// Data Types
+
+// Primitive Type
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail = undefined;
+// let email; // this is same as above "undefined"
+
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// id and anotherId is not same or equal even value of both are same
+// this is what Symbol is used for, making data very unique
+
+// console.log(id === anotherId); // false
+
+// const bigNumber = 222223333355555544444466666888888n; // bigint
+// console.log(typeof bigNumber);
+
+// Reference Type - Non Primitive Type
+// Array, Objects, Function
+
+// array
+// const heros = ["shaktiman", "naagraj", "dooga"];
+// console.log(typeof heros); // objects
+// console.log(heros instanceof Array); // true
+
+// object
+// let myObj = {
+//   name: "sanju",
+//   age: 36,
+// };
+// console.log(typeof myObj); // object
+// console.log(myObj instanceof Object); // true
+
+// function - type "Function Object"
+// const myFunction = function () {
+//   return "hello sanju";
+// };
+// console.log(typeof myFunction); // object
+// console.log(myFunction instanceof Function); // true
