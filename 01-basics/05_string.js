0e5ce30b2395f66376b45cf0c12f9ee1f3b0ed44
omprajapati <omprajapati@gmail.com>
2026-06-24 10:27:32 +0545

    string complete

---
+++ 01-basics/05_string.js
@@ -0,0 +1,40 @@
+// const name = "kritika";
+// const repoCount = 50;
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello, my name is ${name} and my repo count is ${repoCount}.`);
+
+// const gameName = new String("jeshmi");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("s"));
+
+// const newName = "Sanju-Prajapati";
+// const newString = newName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = newName.slice(-15, 4);
+// console.log(anotherString);
+
+// const stringWithSpaces = "     sanju     ";
+// console.log(stringWithSpaces);
+// const stringWithoutSpaces = stringWithSpaces.trim();
+// console.log(stringWithoutSpaces);
+
+// const url = "https://sanju.com/sanju%20prajapati";
+// console.log(url.replace("%20", "-"));
+
+// console.log(url.includes("sanju"));
+// console.log(url.includes("jeshmi"));
+
+const testString = "jeshmi-salina-kritika";
+console.log(testString.split("-"));
+
