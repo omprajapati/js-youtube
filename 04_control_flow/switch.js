c2b18399dc5c870b7b5619933f6ad4446bfd0e45
omprajapati <omprajapati@gmail.com>
2026-06-29 16:17:21 +0545

    truthy done

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,11 +1,39 @@
-const month = 3;
+// const month = 3;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//   // break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   default:
+//     console.log("Defualt case match");
+//     break;
+// }
+
+const month = "mar";
 
 switch (month) {
-  case 1:
+  case "jan":
     console.log("January");
-
     break;
-
+  case "feb":
+    console.log("February");
+    break;
+  case "mar":
+    console.log("March");
+    break;
+  case "apr":
+    console.log("April");
+    break;
   default:
+    console.log("Defualt case match");
     break;
 }
