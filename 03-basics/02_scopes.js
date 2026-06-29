f93ca74ae60e98e3aef48c461fea6000d58f1b92
omprajapati <omprajapati@gmail.com>
2026-06-29 10:28:38 +0545

    scopes finished

---
+++ 03-basics/02_scopes.js
@@ -0,0 +1,21 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// scope {} - curly bracket defines the scope in coding
+// examples - curly brakets in if else, function, for and while loops
+// except {} in object
+
+if (true) {
+  let a = 10; // not accessible from outside
+  const b = 20; // not accessible from outside
+  var c = 30; // accessible from outside
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
