6ca6e5b32cc29ebcf7b240d90bfe5e648f533300
omprajapati <omprajapati@gmail.com>
2026-07-01 11:21:23 +0545

    changes updated

---
+++ 05_iterations/06_six.js
@@ -0,0 +1,13 @@
+// const girls = ["sanju", "eliza", "prashamsa", "srijana", "poudel"];
+
+// const values = girls.forEach((item) => {
+//   //   console.log(item);
+//   return item;
+// });
+
+// console.log(values); // undefined - forEach() doesnot returns value
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let filteredNumbers = myNumbers.filter((num) => num > 4);
+
+console.log(filteredNumbers);
